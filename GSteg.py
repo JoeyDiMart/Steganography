43f28baba084557238eafa5d8ea252d7a7156a95
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2025-04-08 10:23:55 -0400

    finish gsteg

--- GSteg.py
+++ GSteg.py
@@ -3,28 +3,49 @@ Name: Joseph DiMartino
 Program: 8 - GSteg
 Description - Find the needle in the haystack (find hidden data within data)
 '''
+from datetime import datetime
+import os
 
 haystack_filename = "B2097152-duke.mp3.bmp"
-offsets = []  # list of offsets incase data is spread out
+offset = 0  # list offset position
+DLEN = ""
+FNAME = ""
+HSIG = bytearray(b'STEG')  # set HSIG
 #HSIG = bytearray()
 #for char in "STEG":  # uncomment if looking for a different header than "STEG"
 #    HSIG += bytes([ord(char)])
-
-HSIG = bytearray(b'STEG')
 len_header = len(HSIG)
 
-with open(haystack_filename, "rb") as f:
-    haystack = bytearray(f.read())
+with open(haystack_filename, "rb") as rf:
+    haystack = bytearray(rf.read())
     len_haystack = len(haystack)
 
     for i in range(0, len_haystack - len_header):
         if haystack[i:i + len_header] == HSIG:
-            offsets.append(i)
+            print(HSIG.decode('utf-8'))
+            offset = i
             i += len_header  # shift I to after "STEG"
-            FNAME = ""
-            for j in range(i, len_haystack):
+
+            for j in range(i, len_haystack): # find file name (FNAME)
                 if haystack[j:j+1] == bytearray(b'\x00'):
+                    i = j+1
                     break
                 FNAME += chr(haystack[j])
             print(FNAME)
 
+            for j in range(i, len_haystack):
+                if haystack[j:j+3].decode('utf-8') == "L8R":
+                    break
+            DLEN = int.from_bytes(haystack[i:j], byteorder='big')
+            i = j
+            print(DLEN)
+            data = haystack[i:i+DLEN]
+            break
+            # exited for loop since we have all the data we need
+
+    t = datetime.now().strftime("%Y%m%d-%H%M%S")  # get current time stamp
+    FNAME = f"{t}-{FNAME}"  # name a file and write to it
+
+    output_path = os.path.expanduser(f"~/Desktop/CSC330Files/outputs/{FNAME}")
+    with open(f"{output_path}", "wb") as wf:
+        wf.write(data)
