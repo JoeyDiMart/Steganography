cd9b5092ad6224dbe964a83d118e90c810ac593c
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2025-04-07 21:46:14 -0400

    found header and file name

--- GSteg.py
+++ GSteg.py
@@ -6,21 +6,25 @@ Description - Find the needle in the haystack (find hidden data within data)
 
 haystack_filename = "B2097152-duke.mp3.bmp"
 offsets = []  # list of offsets incase data is spread out
-#header = bytearray()
+#HSIG = bytearray()
 #for char in "STEG":  # uncomment if looking for a different header than "STEG"
-#    header += bytes([ord(char)])
+#    HSIG += bytes([ord(char)])
 
-header = bytearray(b'STEG')
-len_header = len(header)
+HSIG = bytearray(b'STEG')
+len_header = len(HSIG)
 
 with open(haystack_filename, "rb") as f:
     haystack = bytearray(f.read())
     len_haystack = len(haystack)
 
     for i in range(0, len_haystack - len_header):
-        if haystack[i:i + len_header] == header:
+        if haystack[i:i + len_header] == HSIG:
             offsets.append(i)
-            print(haystack[i:i+len(header)+30])
             i += len_header  # shift I to after "STEG"
-            print(haystack[i])
-            print(haystack[i:i+10])
+            FNAME = ""
+            for j in range(i, len_haystack):
+                if haystack[j:j+1] == bytearray(b'\x00'):
+                    break
+                FNAME += chr(haystack[j])
+            print(FNAME)
+
